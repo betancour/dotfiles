3e35da0a84bf3fbcc25860021d1120bb74d89f8c
Yitzhak B. Solorzano <you@example.com>
2022-08-26 16:03:25 -0300

    latest changes

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 LOG="${HOME}/Library/Logs/dotfiles.log"
 GITHUB_USER="betancour"
-GITHUB_REPO=""
+GITHUB_REPO="dotfiles"
 USER_GIT_AUTHOR_NAME="Your Name"
 USER_GIT_AUTHOR_EMAIL="you@example.com"
 DIR="/usr/local/opt/${GITHUB_REPO}"
